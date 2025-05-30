ecbeb791f38e89213facd96e9b14ccef8e937bdc
Hafiz <huseynoff.hafiz@gmail.com>
2025-05-30 10:52:09 +0400

    fixed responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -79,12 +79,12 @@ function App() {
   }, [dataCount]);
 
   return (
-    <main className="p-5 flex justify-center">
-      <div className="w-2/3">
-        <h1 className="mb-10 max-w-[500px] mx-auto text-center text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl dark:text-white">
+    <main className="p-2 md:p-5 flex justify-center">
+      <div className="px-5 max-w-full">
+        <h1 className="mb-5 max-w-[500px] mx-auto text-center text-xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl dark:text-white">
           Data Export Demo with Web Worker vs Main Thread
         </h1>
-        <div className="flex justify-center mb-3">
+        <div className="flex justify-center mb-5">
           <Button asChild>
             <a
               href="https://github.com/hafizhuseynov/web-worker"
@@ -101,31 +101,36 @@ function App() {
         <div>
           <ul className="space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
             <li>
-              Export with Web Worker: Runs the export in a background thread.
-              The UI remains responsive, allowing users to continue interacting
-              with the app.
+              <span className="text-gray-800 font-medium">
+                Export with Web Worker
+              </span>
+              : Runs the export in a background thread. The UI remains
+              responsive, allowing users to continue interacting with the app.
             </li>
             <li>
-              Export on Main Thread: Executes the export on the main UI thread,
-              causing the interface to freeze until the operation completes.
+              <span className="text-gray-800 font-medium">
+                Export on Main Thread
+              </span>
+              : Executes the export on the main UI thread, causing the interface
+              to freeze until the operation completes.
             </li>
           </ul>
 
           <blockquote className="p-4 my-4 border-s-4 border-gray-300 bg-gray-50 dark:border-gray-500 dark:bg-gray-800">
-            <p className="text-xl italic font-medium leading-relaxed text-gray-900 dark:text-white">
+            <p className="text-md italic font-medium leading-relaxed text-gray-900 dark:text-white">
               💡 Try clicking the pagination buttons while the export is in
               progress to see the difference in responsiveness
             </p>
           </blockquote>
         </div>
-        <div className="flex flex-col md:flex-row gap-2 justify-end items-center mb-3 mt-5">
-          <div className="flex-1 flex items-center">
-            <label className="whitespace-nowrap me-2">
+        <div className="flex flex-col md:flex-row gap-2 justify-end md:items-end mb-3 mt-5">
+          <div className="flex-1">
+            <label className="whitespace-nowrap me-2 text-xs text-slate-700">
               Data (max. 1,000,000)
             </label>
             <input
               type="number"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               value={dataCount}
               max={1000000}
               onChange={(e) => {
@@ -212,7 +217,9 @@ function App() {
             Export with Worker
           </Button>
         </div>
-        <DataTable data={data} columns={columns} />
+        <div className="overflow-x-hidden max-w-full">
+          <DataTable data={data} columns={columns} />
+        </div>
       </div>
     </main>
   );
